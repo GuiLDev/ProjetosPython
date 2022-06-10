618c0cf8b44aa23c8d405f8d18572191f0d97dcc
GuiLDev <guilhermerebelo.dev@gmail.com>
2022-06-10 16:45:51 -0400

    Utilizando Modelo do Denis

--- Devs Python/Lista.py
+++ Devs Python/Lista.py
@@ -1,22 +1,16 @@
-print('-=-' * 30)
+mediasAlunos = []
+alunosAcimaDaMedia = 0
 
-num_alunos = 3
-nomes = []
-notas = []
-media = 0
+for aluno in range(3):
+	somaDasNotas = 0
+	
+	print(aluno+1, 'º aluno')
+	for nota in range(4):
+		somaDasNotas += float(input("Digite a nota do aluno: "))
+	
+	mediasAlunos.append(somaDasNotas/4)
+	
+	if mediasAlunos[aluno] >= 7:
+		alunosAcimaDaMedia += 1
 
-for i in range(num_alunos):
-    nomes.append(str(input("Informe o nome do aluno: ")))
-    notas.append(float(input("Informe a nota de " + nomes[i] + ': ')))
-    print('~' * 35)
-    media = media + notas[i]
-media = media / num_alunos
-print("A media da turma é {:.2f}" .format(media))
-
-for i in range(num_alunos):
-    if notas[i] > media:
-        print("Parabens!", nomes[i])
-    else:
-        print("Está de recuperação " + nomes[i] + '!')
-        
-print('-=-' * 30)
+print('O número de alunos acima da média é de: ', alunosAcimaDaMedia)

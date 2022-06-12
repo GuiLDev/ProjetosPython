bca840f002d553efc31c4d9a34df2f82c98c1d1e
GuiLDev <guilhermerebelo.dev@gmail.com>
2022-06-12 14:11:35 -0400

    Adicionado alguns comentarios do exercicio

--- Lista de Exercicios 3/Exercicio 1.py
+++ Lista de Exercicios 3/Exercicio 1.py
@@ -1,17 +1,25 @@
 mediasAlunos = []
 alunosAcimaDaMedia = 0
 
+#Faz uma contagem de valor manual entre 3 alunos
+
 for aluno in range(3):
 	somaDasNotas = 0
-	
+ 
+	#Faz uma contagem entre 4 notas para cada aluno 
+ 
 	print(aluno+1, 'º aluno')
 	for nota in range(4):
 		somaDasNotas += float(input("Digite a nota do aluno: "))
 	
+ 	#Divisão das 4 notas por 4 para obter a média
+  
 	mediasAlunos.append(somaDasNotas/4)
 	
 	if mediasAlunos[aluno] >= 7:
 		alunosAcimaDaMedia += 1
-
+  
+	#O Numero de alunos acima da média
+ 
 print('O número de alunos acima da média é de: ', alunosAcimaDaMedia)
 

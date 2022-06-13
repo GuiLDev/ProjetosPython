9c9261142a428d64b18056bf2be27ddb839f9b3b
GuiLDev <guilhermerebelo.dev@gmail.com>
2022-06-13 16:02:05 -0400

    adição da lista Quad

---
+++ Lista de Exercicios 3/Exercicio 4.py
@@ -0,0 +1,24 @@
+num1 = []
+quad = []
+mulp = 0
+
+for x in range(10):
+     num1.append(int(input("Digite um número inteiro: ")))
+for i in num1:
+    mulp = mulp + num1[x] ** 2
+    quad.append(mulp)
+    
+print('Os números da lista é: ', num1)
+print('A soma dos número elevados ao quadrado é: ', mulp)
+    
+     
+     
+     
+     
+        
+     
+     
+     
+
+
+     

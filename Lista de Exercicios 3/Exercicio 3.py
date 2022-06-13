72f7500b8b243b06c6124ecefd61a60158900fcf
GuiLDev <guilhermerebelo.dev@gmail.com>
2022-06-13 12:41:07 -0400

    Adicionado codigo reverter altura e idade

---
+++ Lista de Exercicios 3/Exercicio 3.py
@@ -0,0 +1,13 @@
+idade = []
+altura = []
+a = 0
+
+for a in range(5):
+    print(a+1, '° Pessoa')
+    altura.append(float(input("Qual é a altura da pessoa? ")))
+    idade.append(int(input('Qual é a idade da pessoa? ')))
+
+print('idade normal', idade)
+print('idade invertida', (idade[::-1]))
+print('altura normal', altura)
+print('altura invertida', (altura[::-1]))
